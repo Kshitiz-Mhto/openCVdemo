d705ac11d4aaa8818147ab8d2af108ac1e4fd484
Kshitiz Mhto <kshitizmhto101@gmail.com>
2024-02-14 10:33:51 +0545

    feature: added switching of camera

--- app/src/main/java/com/sarvanam/opencvdemo/MainActivity.java
+++ app/src/main/java/com/sarvanam/opencvdemo/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.Manifest;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 import com.sarvanam.opencvdemo.databinding.ActivityMainBinding;
@@ -43,6 +44,7 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
     private CascadeClassifier faceCascade;
     private Mat mRgba;
     private Mat mGray;
+    private int cameraMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +52,26 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(binding.getRoot());
+        cameraMode = Camera.CameraInfo.CAMERA_FACING_BACK;
         if (!hasPermissions(PERMISSIONS)) {
             // Request permissions
             ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_PERMISSION);
         }else {
             if (OpenCVLoader.initDebug()) {
-                Toast.makeText(getApplicationContext(),"OpenCV Successfully initialized",Toast.LENGTH_SHORT).show();
                 faceCascade = new CascadeClassifier();
                 try {
                     setFaceCascade();
+                    initCamera();
+                    Toast.makeText(getApplicationContext(),"OpenCV Successfully initialized",Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                initCamera();
+                binding.btnSwitchCamera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        switchCamera();
+                    }
+                });
             } else {
                 Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG).show();
                 return;
@@ -88,7 +97,7 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
         mOpenCvCameraView = binding.camView;
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
-        mOpenCvCameraView.setCameraIndex(Camera.CameraInfo.CAMERA_FACING_FRONT);
+//        mOpenCvCameraView.setCameraIndex(cameraMode);
         mOpenCvCameraView.enableView();
     }
 
@@ -112,19 +121,39 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
         MatOfRect faces = new MatOfRect();
         faceCascade.detectMultiScale(mGray, faces);
 
+        // Get the orientation of the camera frame
+        int orientation = inputFrame.rgba().rows() > inputFrame.rgba().cols() ? 90 : 0;
+
         // Draw bounding boxes around detected faces with increased size
         for (Rect rect : faces.toArray()) {
-            int increaseSize = 20;
-            Rect enlargedRect = new Rect((int) (rect.tl().x - increaseSize),
-                    (int) (rect.tl().y - increaseSize),
-                    rect.width + 2 * increaseSize,
-                    rect.height + 2 * increaseSize);
-            Imgproc.rectangle(mRgba, enlargedRect.tl(), enlargedRect.br(), new Scalar(255, 0, 0), 3);
+            // Increase the size of the rectangle
+            int increaseSize = 20; // You can adjust this value to increase/decrease the size
+            // Adjust coordinates based on orientation
+            Rect adjustedRect;
+            if (orientation == 90) {
+                adjustedRect = new Rect((int) (rect.tl().y - increaseSize),
+                        (int) (mRgba.cols() - rect.br().x - increaseSize),
+                        rect.height + 2 * increaseSize,
+                        rect.width + 2 * increaseSize);
+            } else {
+                adjustedRect = new Rect((int) (rect.tl().x - increaseSize),
+                        (int) (rect.tl().y - increaseSize),
+                        rect.width + 2 * increaseSize,
+                        rect.height + 2 * increaseSize);
+            }
+
+            Imgproc.rectangle(mRgba, adjustedRect.tl(), adjustedRect.br(), new Scalar(255, 0, 0), 3);
         }
 
         return mRgba;
     }
 
+    public void switchCamera() {
+        cameraMode = (cameraMode + 1) % 2; // toggle between 0 and 1
+        mOpenCvCameraView.disableView();
+        mOpenCvCameraView.setCameraIndex(cameraMode);
+        mOpenCvCameraView.enableView();
+    }
 
     @Override
     public void onPause()
@@ -139,7 +168,7 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
     {
         super.onResume();
         if (mOpenCvCameraView != null)
-            mOpenCvCameraView.enableView();
+            switchCamera();
     }
 
     @Override
