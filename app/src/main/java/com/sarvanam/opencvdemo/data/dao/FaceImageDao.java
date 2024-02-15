d18c51176fd8977f3e0a9fae38d94964d6de9667
Kshitiz Mhto <kshitizmhto101@gmail.com>
2024-02-15 12:04:58 +0545

    feat: added retriving method in dao layer

--- app/src/main/java/com/sarvanam/opencvdemo/data/dao/FaceImageDao.java
+++ app/src/main/java/com/sarvanam/opencvdemo/data/dao/FaceImageDao.java
@@ -3,12 +3,20 @@ package com.sarvanam.opencvdemo.data.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
 
 import com.sarvanam.opencvdemo.data.modal.FaceImage;
+
+import java.util.List;
 
 // Define the Room DAO (Data Access Object)
 @Dao
 public interface FaceImageDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(FaceImage faceImage);
+
+    @Transaction
+    @Query("select * from face_images")
+    List<FaceImage> getAllFaceImages();
 }
